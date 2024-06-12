94abfcefdbcf69090e4d5d5dedb64b63baa46d00
Gueren Lanelle <guerenla@ge-f4.kzn.21-school.ru>
2024-06-12 13:18:51 +0300

    test jwt

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 from config import Config
+import joblib
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,45 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+model_grant = joblib.load('models/itis_grant_model.pkl')
+
+model_student_performance = joblib.load('models/student_performance_model.pkl')
+
+
+@app.route('/grant_predict', methods=['POST'])
+@jwt_required()
+def grant_predict():
+    data = request.json
+    average_score = data['average_score']
+    grant_student_count = data['grant_student_count']
+    grant_student_applied = data['grant_student_applied']
+
+    input_data = [[average_score, grant_student_count, grant_student_applied]]
+
+    prediction = model_grant.predict(input_data)
+
+    response = {'prediction': prediction[0]}
+    return jsonify(response), 200
+
+
+@app.route('/predict_student_score', methods=['POST'])
+@jwt_required()
+def predict_student_score():
+    data = request.json
+    m_edu = data['mEdu']
+    f_edu = data['fEdu']
+    study_time = data['studyTime']
+    failures = data['failures']
+    support = data['support']
+    higher = data['higher']
+    absences = data['absences']
+
+    input_data = [[m_edu, f_edu, study_time, failures, support, higher, absences]]
+    prediction = model_student_performance.predict(input_data)
+
+    response = {'score': prediction[0]}
+    return jsonify(response), 200
 
 
 @app.route('/register', methods=['POST'])
